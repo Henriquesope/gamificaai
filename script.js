b3d6d922e24039cd112a843d25510c373a5e389a
HENRIQUE SOARES PEREIRA <48878495832@info.local>
2024-05-24 10:54:31 -0300

    json

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let menu = document.getElementById("menu")
 let iconeBarras = document.getElementById("icone-barras")
 let iconeX = document.getElementById("icone-x")
 
-function abreFechaMenu(){
+function abreFechaMenu() {
     //menu fechado tem a class "menu-fechado"
     //menu aberto ele nao tem a class "menu fechado"
 
-    
 
 
-    if (menu.classList.contains("menu-fechado")){
+
+    if (menu.classList.contains("menu-fechado")) {
         // Abrir o menu
         menu.classList.remove("menu-fechado")
 
@@ -24,7 +24,7 @@ function abreFechaMenu(){
         //mostrar icone x
         iconeX.style.display = "none"
         //esconder o icone de barras
-        iconeBarras.style.display= "inline"
+        iconeBarras.style.display = "inline"
     }
 }
 
@@ -64,23 +64,23 @@ function mostrarProximoSlide() {
     else {
         slideAtual = 0
     }
-    
+
     //mostrar slide de acordo com o slide 
     banner.classList.add(slides[slideAtual])
 }
 
 function mostrarSlideAnterior() {
-       // remover o slide anterior
-       banner.classList.remove(slides[slideAtual])
-        //subtrair 1 na variavel slideAtual
-        if (slideAtual > 0) {
-            slideAtual--
+    // remover o slide anterior
+    banner.classList.remove(slides[slideAtual])
+    //subtrair 1 na variavel slideAtual
+    if (slideAtual > 0) {
+        slideAtual--
 
-        } else {
-            slideAtual = 2
-        }
-        
-       //mostrar slide de acordo com o slide 
+    } else {
+        slideAtual = 2
+    }
+
+    //mostrar slide de acordo com o slide 
     banner.classList.add(slides[slideAtual])
 }
 
@@ -92,4 +92,30 @@ function selecionarSlide(indiceslide) {
     slideAtual = indiceslide
     //mostra o slide selecionado e salvo na variavel
     banner.classList.add(slides[slideAtual])
+}
+
+
+//CARREGAMENTO DINAMICO DOS CASES
+let listaCases = [
+
+]
+
+function renderizarCases() {
+    //encontrar o elemento para inserir os cards
+    let containerCards = document.querySelector(".container-cards")
+
+    let template = ""
+    //para cada case da lista cases
+    listaCases.forEach(cardCase => {
+        //Montar o html do card, passando os atributos do case
+        template += ` <div class="card">
+        <img src=${cardCase.imagem}alt="">
+        <p>${cardCase.descricao }</p>
+        <button>Ver mais</button>
+    </div>`
+    })
+
+
+    //inserir html dos cases montados no elemento container-cards
+    containerCards.innerHTML = template
 }
